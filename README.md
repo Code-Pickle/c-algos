4722a3c73b351533d6d63f1f447f1cc83b8850a9
Arnav Dixit <arnavdixit127@gmail.com>
2019-10-23 06:41:39 +0530

    Added selecton Sort
    Added sorting.cpp and sorting.h and modified README.md in a way to accommodate more sorting algorithms.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C Algorithms
 A collection of algorithms written in C.
+
+## Sorting Algorithms
+- Selection Sort
---
+++ sorting.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//--------Selection Sort----------//
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_id;
+	for (i = 0; i < n-1; i++)
+	{
+		min_id = i;
+		for (j = i+1; j < n; j++)
+		    if (arr[j] < arr[min_id])
+			    min_id = j;
+        int temp = arr[min_id];
+        arr[min_id]= arr[i];
+        arr[i]=temp;
+	}
+}
+
+
+
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
