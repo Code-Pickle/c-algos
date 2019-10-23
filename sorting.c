ed360b8db70f3a81e10d8d00d78257c97d392aa4
Seshal Jain <seshaljain11@gmail.com>
2019-10-23 15:07:17 +0530

    Implement CS50's uniform formatting

--- sorting.c
+++ sorting.c
@@ -2,21 +2,24 @@
 
 int main()
 {
-	int arr[100], a=0, x, opt;
+    int arr[100], a = 0, x, opt;
     char c[10];
-    printf("Enter elements:(0 to stop Entering) \n");
+    printf("Enter elements:(0 to stop entering) \n");
     do
     {
         scanf("%d", &x);
-        if(x!=0)
-            arr[a++]=x;
-    }while(x!=0);
-	int n = sizeof(arr)/sizeof(arr[0]);
+        if (x != 0)
+        {
+            arr[a++] = x;
+        }
+    }
+    while (x != 0);
+    int n = sizeof(arr) / sizeof(arr[0]);
     printf("Choose sorting algorithm:\n");
-    printf("1)Selection Sort\n");
-    printf("Enter option(0 to exit): ");
+    printf("1) Selection Sort\n");
+    printf("Enter option (0 to exit): ");
     scanf("%d", &opt);
-    switch(opt)
+    switch (opt)
     {
         case 1:
             selectionSort(arr, a);
@@ -24,7 +27,7 @@ int main()
         default:
             return 0;
     }
-	printf("\n\nSorted array: \n");
-	printArray(arr, a);
-	return 0;
+    printf("\n\nSorted array: \n");
+    printArray(arr, a);
+    return 0;
 }
--- sorting.h
+++ sorting.h
@@ -3,26 +3,27 @@
 //--------Selection Sort----------//
 void selectionSort(int arr[], int n)
 {
-	int i, j, min_id;
-	for (i = 0; i < n-1; i++)
-	{
-		min_id = i;
-		for (j = i+1; j < n; j++)
-		    if (arr[j] < arr[min_id])
-			    min_id = j;
+    int i, j, min_id;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_id = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_id])
+            {
+                min_id = j;
+            }
         int temp = arr[min_id];
-        arr[min_id]= arr[i];
-        arr[i]=temp;
-	}
+        arr[min_id] = arr[i];
+        arr[i] = temp;
+    }
 }
-
-
-
 
 void printArray(int arr[], int size)
 {
-	int i;
-	for (i=0; i < size; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
